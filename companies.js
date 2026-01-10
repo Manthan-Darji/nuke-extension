a6ab5c167a6e07a689aa80ee766f339774378708
Manthan-Darji <solankimanthan22@gmail.com>
2026-01-10 16:18:17 +0530

    Third  Commit

--- companies.js
+++ companies.js
@@ -1,6 +1,6 @@
 // A simple list of targets. Add as many as you want.
 const TARGETS = [
-    { name: "Facebook", email: "privacy@facebook.com" },
+    { name: "Facebook", email: "mutuallyunknown@gmail.com" },
     { name: "Zomato", email: "privacy@zomato.com" },
     { name: "LinkedIn", email: "privacy@linkedin.com" },
     { name: "Twitter/X", email: "dpo@twitter.com" },
